53435b1e073ede7e0db57d99bcfe1c935b26b460
Mahesh Yadav <ymahesh756@gmail.com>
2023-10-17 07:45:33 +0530

    Query string

--- src/app/api/getstockprice/route.js
+++ src/app/api/getstockprice/route.js
@@ -6,19 +6,60 @@
 
 
 
-async function GET() {
+async function GET(req) {
+    const searchParams = req.nextUrl.searchParams;
+    const stockName = searchParams.get('stockName')
 
     //await
 
 
     // every function return something
-    const min = 124;
-    const max = 126;
-    const stockPrice = parseFloat((Math.random() * (max - min) + min).toFixed(2));
+    let min, max, stockPrice, resData;
 
-    return Response.json({
-        price: stockPrice,
-    });
+
+
+    switch (stockName) {
+        case 'idfc':
+            min = 124;
+            max = 126;
+            stockPrice = parseFloat((Math.random() * (max - min) + min).toFixed(2));
+            resData = {
+                //property:value
+                stockName,
+                price: stockPrice,
+            }
+
+            break;
+        case 'icici':
+            min = 800;
+            max = 1000;
+            stockPrice = parseFloat((Math.random() * (max - min) + min).toFixed(2));
+            resData = {
+                //property:value
+                stockName,
+                price: stockPrice,
+            }
+
+            break;
+        case 'hdfc':
+            min = 1400;
+            max = 1600;
+            stockPrice = parseFloat((Math.random() * (max - min) + min).toFixed(2));
+            resData = {
+                //property:value
+                stockName,
+                price: stockPrice,
+            }
+
+            break;
+        default:
+            resData = {
+                //property:value
+                msg:'Invalid Stock Name'
+            }
+    }
+
+    return Response.json(resData);
 }
 
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,52 +1,78 @@
 "use client"
 //1. Import area
 
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material'
-import { useState } from 'react'
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import { useEffect, useState } from 'react'
 
 
 //2. Define area
 export default function Home() {
   //2.1 Hooks area
+  const [stockName, setStockName] = useState('');
   const [selectValue, setSelectValue] = useState('');
   const [stockPrice, setStockPrice] = useState('')
 
-  //function Defination area
+  useEffect(()=>{
+    setInterval(function (e) {
+      
+      // console.log(e.target.value);
+      fetch(`/api/getstockprice?stockName=${stockName}`).then((res) => {
+        return res.json()
+      }).then((data) => {
+        console.log(data);
+        console.log(data.price);
+        setStockPrice(data.price)
+  
+      }).catch((err) => {
+  
+      }).finally(() => {
+  
+      })
+    }, 60000);
+  },[stockName])
+  
+  // 2.2 function Defination area
   const handleChange = (e) => {
-              //object.property.property
+    //object.property.property
+    setStockName(e.target.value);
     console.log(e.target.value);
+    
     // This is called promiseChain
-    fetch('/api/getstockprice').then((res)=>{
+    fetch(`/api/getstockprice?stockName=${e.target.value}`).then((res) => {
       return res.json()
-    }).then((data)=>{
+    }).then((data) => {
       console.log(data);
       console.log(data.price);
       setStockPrice(data.price)
 
-    }).catch((err)=>{
+    }).catch((err) => {
 
-    }).finally(()=>{
+    }).finally(() => {
 
     })
-    // Now call the API
-  } 
+    
+  }  //new way two define a function Es6 (2015)
+
+  //2.3 return statement
   return (
     <main>
-      <h1>Current price of below stock is {stockPrice} </h1>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Stock Market</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          label="Stock Price"
-          value={selectValue}
-          onChange={handleChange}
-        >
-          <MenuItem value="idfc">IDFC LIMITED</MenuItem>
-          <MenuItem value="icici">ICICI BANK LIMITED</MenuItem>
-          <MenuItem value="hdfc">HDFC BANK LIMITED</MenuItem>
-        </Select>
-      </FormControl>
+      <Box sx={{ minWidth: '90%', }}>
+        <h1>Current price of below stock is {stockPrice} </h1>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Stock Name</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            label="Stock Price"
+            value={selectValue}
+            onChange={handleChange}
+          >
+            <MenuItem value="idfc">IDFC LIMITED</MenuItem>
+            <MenuItem value="icici">ICICI BANK LIMITED</MenuItem>
+            <MenuItem value="hdfc">HDFC BANK LIMITED</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
     </main>
   )
 }
